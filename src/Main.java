d75cea1df621e2f2b197dd2384adaea84e7b1c1e
Анастасия <nikitina2022@gmail.com>
2023-02-01 22:38:19 +0500

    Второй комид

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+public class Main {
+    public static void main(String[] args) {
+        //write your code here
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        var WeightOfOneBoxer = 78.2;
+        System.out.println(WeightOfOneBoxer);
+        var WeightOfTheSecondBoxer = 82.7;
+        System.out.println(WeightOfTheSecondBoxer);
+        var TotalWeight = WeightOfOneBoxer + WeightOfTheSecondBoxer;
+        System.out.println(TotalWeight);
+        var Weightdifference = WeightOfTheSecondBoxer - WeightOfOneBoxer;
+        System.out.println("Вес второго спортсмена больше на " + Weightdifference + " кг!" );
+        var WeightDifference = 4.5;
+        var overweight = (WeightOfTheSecondBoxer + WeightOfOneBoxer) % WeightDifference;
+        System.out.println(overweight);
+
+
+
+
+
+
+
+    }
+}
